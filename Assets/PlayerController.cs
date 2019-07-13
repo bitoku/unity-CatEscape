f80843f7bc95e34e73b36c2766aaafcc7533b7dd
Ayato Tokubi <tokubi.ayato@gmail.com>
2019-07-13 15:49:27 +0900

    initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public void LeftButtonDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+
+    public void RightButtonDown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+}
